daca285494e4d302b93cbd8d287564a6fae30d00
Andrey <raigon28@gmail.com>
2021-06-30 02:12:25 +0300

    AOS and Smooth Scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
+// Slider
 const swiper = new Swiper('.swiper-container', {
     pagination: {
         el: '.swiper-pagination',
     },
+    loop: true,
     grabCursor: true,
     keyboard: {
         enabled: true,
@@ -9,4 +11,20 @@ const swiper = new Swiper('.swiper-container', {
         pageUpDown: true,
     },
     autoHeight: true,
+});
+
+// Animate On Scroll
+AOS.init({
+    duration: 800
+});
+
+// Smooth Scroll To Anchor
+document.querySelectorAll('a[href^="#anchor"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
